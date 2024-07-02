a3cd2ec87878db4e015c702f09a2d74898a0d3a2
El0Lobo <95318796+El0Lobo@users.noreply.github.com>
2024-07-02 04:47:40 +0200

    changing dishes

---
+++ dishes.js
@@ -0,0 +1,25 @@
+// dishes.js
+export const dishes = [
+    // 0: Bratwurst mit Potato Wedges
+    {
+        name: "Bratwurst mit Potato Wedges",
+        image: "pics/Layer1.png",
+        priceDineIn: "7,60 €",
+        priceToGo: "To Go: <br> 6,90 €"
+    },
+    // 1: Schnitzel mit Pommes
+    {
+        name: "Schnitzel mit Pommes",
+        image: "pics/Layer2.png",
+        priceDineIn: "8,50 €",
+        priceToGo: "To Go: <br> 7,80 €"
+    },
+    // 3: Rouladen mit Kartoffeln
+    {
+        name: "Rouladen mit Kartoffeln",
+        image: "pics/Layer3.png",
+        priceDineIn: "6,50 €",
+        priceToGo: "To Go: <br> 5,80 €"
+    },
+    // Add more dishes here
+];
